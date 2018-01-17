7a0921a3fdfc45bdd3a017c0ca251cf286641824
Tim <tim.hanssen@doelgroep.tv>
2018-01-17 13:15:33 +0100

    Update TwitterOAuth.php

--- src/TwitterOAuth.php
+++ src/TwitterOAuth.php
@@ -18,6 +18,7 @@ class TwitterOAuth extends Config
     const API_VERSION = '1.1';
     const API_HOST = 'https://api.twitter.com';
     const UPLOAD_HOST = 'https://upload.twitter.com';
+    const UPLOAD_CHUNK = 4500000; // 1024 * 40 = 40960
 
     /** @var Response details about the result of the last request */
     private $response;
